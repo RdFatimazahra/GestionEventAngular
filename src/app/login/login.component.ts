0e881c0c39b52174057ad2e15fc819f5af84bb5a
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-07-31 09:48:20 +0100

    Réaliser partie login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { JwtService } from '../service/jwt.service';
+import { Router } from '@angular/router';
+import { Jwt } from '../model/jwt';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  loginForm!: FormGroup;
+  constructor(
+    private service: JwtService,
+    private fb: FormBuilder,
+    private router: Router
+  
+  ){}
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    })
+  }
+  submitForm(): void {
+    console.log(this.loginForm.value);
+    this.service.login(this.loginForm.value).subscribe(
+      (response : Jwt) => {
+             const jwToken = response.token;
+            localStorage.setItem('jwt', jwToken);
+           this.router.navigateByUrl("/dashbord")
+        }
+    )
+  }
 
 }
